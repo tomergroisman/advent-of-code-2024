e953c1a88c72be5c832b39d05bcd6634821ae462
Tomer Groisman <tomergroisman@gmail.com>
2024-12-14 12:46:39 +0200

    refactor: day 9

--- days/day9/solution.py
+++ days/day9/solution.py
@@ -1,103 +1,80 @@
 import sys
 from pathlib import Path
 
-type MemoryData = tuple[int, int]
+type MemoryData = tuple[int | None, int]
 
 
 def part_1(raw_input: str) -> float:
     disk_map = parse_input(raw_input)
-    order_free_space(disk_map)
+    free_space(disk_map)
     return calc_checksum(disk_map)
 
 
 def part_2(raw_input: str) -> float:
     disk_map = parse_input(raw_input)
-    order_free_space_blocks(disk_map)
-    # print(disk_map)
+    free_space_blocks(disk_map)
     return calc_checksum(disk_map)
 
 
 def parse_input(raw_input: str) -> list[MemoryData]:
     return [
-        (i // 2 if i % 2 == 0 else -1, int(data))
+        (i // 2 if i % 2 == 0 else None, int(data))
         for i, data in enumerate(list(raw_input.strip()))
     ]
 
 
-def swap(
-    disk_map: list[MemoryData],
-    free_space_idx: int,
-    full_space_idx: int,
-    local_swap: bool,
-):
-    free_space, full_space = disk_map[free_space_idx], disk_map[full_space_idx]
-    is_full_contained = full_space[1] < free_space[1]
-    is_full_overflow = full_space[1] > free_space[1]
-    if is_full_contained:
-        disk_map[free_space_idx] = (free_space[0], free_space[1] - full_space[1])
-        disk_map.pop(full_space_idx)
-        disk_map.insert(free_space_idx, full_space)
-        if local_swap:
-            disk_map.insert(full_space_idx, (free_space[0], full_space[1]))
+def free_space(disk_map: list[MemoryData]):
+    free_i, file_i = 1, len(disk_map) - 1
+    while free_i < file_i:
+        free_block, file_block = disk_map[free_i], disk_map[file_i]
+        will_be_contained = file_block[1] < free_block[1]
+        will_fit = file_block[1] == free_block[1]
+        if will_be_contained:
+            disk_map[free_i] = (file_block[0], file_block[1])
+            disk_map[file_i] = (None, file_block[1])
+            disk_map.insert(free_i + 1, (None, free_block[1] - file_block[1]))
+            file_i -= 1
+            free_i += 1
+        elif will_fit:
+            disk_map[free_i], disk_map[file_i] = disk_map[file_i], disk_map[free_i]
+            file_i -= 2
+            free_i += 2
         else:
-            disk_map.append((free_space[0], full_space[1]))
-        return free_space_idx + 1, full_space_idx - 1
-    elif is_full_overflow:
-        disk_map.append(free_space)
-        disk_map[full_space_idx] = (full_space[0], full_space[1] - free_space[1])
-        disk_map[free_space_idx] = (full_space[0], free_space[1])
-        return free_space_idx + 2, full_space_idx
-    else:
-        disk_map[free_space_idx], disk_map[full_space_idx] = (
-            disk_map[full_space_idx],
-            disk_map[free_space_idx],
-        )
-        return free_space_idx + 2, full_space_idx - 2
+            disk_map[free_i] = (file_block[0], free_block[1])
+            disk_map[file_i] = (file_block[0], file_block[1] - free_block[1])
+            disk_map.insert(file_i + 1, (None, free_block[1]))
+            free_i += 2
 
 
-def order_free_space(disk_map: list[MemoryData]):
-    current_free_idx = 1
-    current_full_idx = len(disk_map) - 1
-    while current_free_idx < current_full_idx:
-        current_free_idx, current_full_idx = swap(
-            disk_map, current_free_idx, current_full_idx, False
-        )
+def free_space_blocks(disk_map: list[MemoryData]):
+    file_i = len(disk_map) - 1
+    while file_i > 0:
+        file_block = disk_map[file_i]
 
+        for i, block in enumerate(disk_map[:file_i]):
+            if block[0] is not None:
+                continue
+            free_block, free_i = block, i
+            will_be_contained = file_block[1] < free_block[1]
+            will_fit = file_block[1] == free_block[1]
+            if will_be_contained:
+                disk_map[free_i] = (file_block[0], file_block[1])
+                disk_map[file_i] = (None, file_block[1])
+                disk_map.insert(free_i + 1, (None, free_block[1] - file_block[1]))
+                break
+            elif will_fit:
+                disk_map[free_i], disk_map[file_i] = disk_map[file_i], disk_map[free_i]
+                break
 
-def order_free_space_blocks(disk_map: list[MemoryData]):
-    visited = set()
-    current_full_idx = len(disk_map) - 1
-    while current_full_idx is not None:
-        visited.add(disk_map[current_full_idx][0])
-        full_space = disk_map[current_full_idx]
-        free_space_idx = next(
-            (
-                free_space_idx
-                for free_space_idx, data in enumerate(disk_map[:current_full_idx])
-                if (data[0] == -1 and full_space[1] <= data[1])
-            ),
-            None,
-        )
-        if free_space_idx is not None:
-            swap(disk_map, free_space_idx, current_full_idx, True)
-
-        current_full_idx = next(
-            (
-                len(disk_map) - 1 - i
-                for i, data in enumerate(disk_map[::-1])
-                if data[0] != -1 and not data[0] in visited
-            ),
-            None,
-        )
+        file_i = file_i - 1 if disk_map[file_i - 1][0] is not None else file_i - 2
 
 
 def calc_checksum(disk_map: list[MemoryData]):
-    res, current_idx = 0, 0
-    for memory_data in disk_map:
-        for block in range(memory_data[1]):
-            if memory_data[0] != -1:
-                res += memory_data[0] * current_idx
-            current_idx += 1
+    res, i = 0, 0
+    for data in disk_map:
+        for block in range(data[1]):
+            res += i * (data[0] or 0)
+            i += 1
     return res
 
 
